ea23190a7f12a2db90809f08a068a4fcc3051796
Michael Yockey <myockey@cirrusaircraft.com>
2024-10-28 13:20:51 -0400

    nvim: Add binding to remove search highlight

--- nvim/.config/nvim/lua/yock/remap.lua
+++ nvim/.config/nvim/lua/yock/remap.lua
@@ -1,6 +1,9 @@
 -- Move highlights
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv") 
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Remove search highlight
+vim.keymap.set('n', '<leader>/', ':nohl<CR>')
 
 -- Paste over without copying what is under
 vim.keymap.set('x', '<leader>p', "\"_dP")
