15d4c5373ab2de3cdde6211c92e3bf32a53b1e46
Michael Yockey <mike@mikeyockey.com>
2025-02-11 21:20:29 -0500

    nvim: Fix completions

--- nvim/.config/nvim/lua/yock/plugins/lsp.lua
+++ nvim/.config/nvim/lua/yock/plugins/lsp.lua
@@ -3,16 +3,56 @@ return {
   dependencies = {
     'hrsh7th/nvim-cmp',
     'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'L3MON4D3/LuaSnip',
   },
   config = function()
     local lspconfig = require('lspconfig')
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    local cmp = require('cmp')
+    local luasnip = require('luasnip')
+    require('luasnip.loaders.from_vscode').lazy_load()
+
+    -- Completion
+    luasnip.filetype_extend('ruby', { 'rails' })
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-j>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-k>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+      }, {
+        { name = 'buffer' },
+      })
+    })
+
+    -- LSP
     capabilities.textDocument.completion.completionItem.snippetSupport = true
 
     lspconfig.pylsp.setup({ capabilities = capabilities })
     lspconfig.ts_ls.setup({ capabilities = capabilities })
     lspconfig.ruby_lsp.setup({ capabilities = capabilities })
     lspconfig.angularls.setup({ capabilities = capabilities })
+    lspconfig.ols.setup({
+      capabilities = capabilities,
+      init_options = {
+        checker_args = "-strict-style"
+      },
+    })
     lspconfig.emmet_ls.setup({
       capabilities = capabilities,
       filetypes = {
@@ -23,8 +63,6 @@ return {
         'sass',
         'javascript',
         'typescript',
-        'javascriptreact',
-        'typescriptreact'
       },
     })
     lspconfig.lua_ls.setup({
