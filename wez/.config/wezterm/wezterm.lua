9a90772ef79aee7f575a07e9af9958317d9dbc0e
Michael Yockey <myockey@cirrusaircraft.com>
2025-02-13 15:44:07 -0500

    wez: Use Tempus Past theme

--- wez/.config/wezterm/wezterm.lua
+++ wez/.config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require('wezterm')
 local config = wezterm.config_builder()
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Tempus Past"
 config.font = wezterm.font('TX-02')
 config.font_size = 17
 
