8de8148b031ce87c63a1f229ae0cd2c81aaf0714
Michael Yockey <myockey@cirrusaircraft.com>
2025-02-13 10:28:36 -0500

    nvim: Unify Telescope config

--- nvim/.config/nvim/lua/yock/plugins/telescope.lua
+++ nvim/.config/nvim/lua/yock/plugins/telescope.lua
@@ -1,6 +1,12 @@
 return {
   'nvim-telescope/telescope.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    }
+  },
   config = function()
     local telescope = require('telescope')
     local builtin = require('telescope.builtin')
