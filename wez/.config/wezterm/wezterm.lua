e7401fc495d402d24c88a8fc97d7f1649d72a3fd
Michael Yockey <mike@mikeyockey.com>
2025-02-13 20:59:20 -0500

    wez: Dark mode
    Adds a dark mode theme and setups automatic theme switching based on system light and dark mode.

--- wez/.config/wezterm/wezterm.lua
+++ wez/.config/wezterm/wezterm.lua
@@ -1,7 +1,23 @@
 local wezterm = require('wezterm')
 local config = wezterm.config_builder()
-config.color_scheme = "Tempus Past"
 config.font = wezterm.font('TX-02')
 config.font_size = 17
+
+local function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+local function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Tempus Classic'
+  else
+    return 'Tempus Past'
+  end
+end
+
+config.color_scheme = scheme_for_appearance(get_appearance())
 
 return config
